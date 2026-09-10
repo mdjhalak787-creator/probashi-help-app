dbc0756387936f7effc61ce9b13e93a8f3f8363d
mdjhalak787-creator <mdjhalak787@gmail.com>
2026-09-10 08:56:32 +0600

    Refactor App component and update home page text

--- App.tsx
+++ App.tsx
@@ -2,14 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminDashboard from './AdminDashboard';
 import SuperAdminDashboard from './SuperAdminDashboard';
 import AgentDashboard from './AgentDashboard';
-// আপনার অন্যান্য পেজগুলো ইমপোর্ট করে নেবেন
 
-function App() {
+export default function App() {
   return (
     <Router>
       <div className="min-h-screen bg-gray-900 text-white font-sans">
         <Routes>
-          <Route path="/" element={<div className="p-4">হোম পেজ</div>} />
+          <Route path="/" element={<div className="p-6 text-xl">Probashi Help Iraq - Home</div>} />
           <Route path="/admin" element={<AdminDashboard />} />
           <Route path="/super-admin" element={<SuperAdminDashboard />} />
           <Route path="/agent" element={<AgentDashboard />} />
@@ -18,5 +17,3 @@ function App() {
     </Router>
   );
 }
-
-export default App;
